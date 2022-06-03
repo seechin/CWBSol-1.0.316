171b6fce00dd0e340cb3c289b9ba1bcf8a8b0ed7
Siqin Cao <31610952+seechin@users.noreply.github.com>
2022-06-03 10:06:03 -0400

    1.0.315

--- page_footer.php
+++ page_footer.php
@@ -3,7 +3,7 @@
     $server = $_SERVER['HTTP_HOST'];
 
     echo ('<div class="container"><div class="footercontainer">');
-    echo ('<font size=2>'.$software_name.(empty($server)?'':' on <a href="/" style="color:blue">'.$server.'</a> ').'© Siqin Cao 2020</font>');
+    echo ('<font size=2>'.$software_name.(empty($server)?'':' on <a href="/" style="color:blue">'.$server.'</a> ').'(Siqin Cao, 2022)</font>');
     echo ('</div></div>');
     echo ("</body></html>")
 
